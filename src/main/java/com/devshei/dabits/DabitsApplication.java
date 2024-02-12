503dd9ca31ae926587a7cac1947c2e5dc0ba0834
Sheila Diz <dizsheila07@gmail.com>
2024-02-12 20:04:19 -0300

    first commit

---
+++ src/main/java/com/devshei/dabits/DabitsApplication.java
@@ -0,0 +1,13 @@
+package com.devshei.dabits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DabitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DabitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devshei/dabits/DabitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devshei.dabits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DabitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
